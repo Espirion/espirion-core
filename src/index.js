68459777ce02d14c871f237fe224f170ae155ad4
Kothapalli Somasekhar <somunani2413@gmail.com>
2025-05-31 15:46:59 +0530

    refactor: remove unused components and update index.html for improved button showcase

--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
-import "./components/esp-backdrop.js";
 import "./components/esp-button.js";
-import "./components/esp-button-group.js";
-
 import "./components/esp-icon.js";
-
-import "./components/esp-tooltip.js";
